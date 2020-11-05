a21d235bd89cdc2b8bdb746365a38c850c526391
MichaelBad <chistol.mvp@gmail.com>
2020-11-05 19:45:28 +0200

    change format number

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const newYear = '1 Jan 2021';
-const daysEl = document.querySelector('#days')
-const hoursEl = document.querySelector('#hours')
-const minutesEl = document.querySelector('#minutes')
-const secondsEl = document.querySelector('#seconds')
+const daysEl = document.querySelector('#days');
+const hoursEl = document.querySelector('#hours');
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
 const spinner = document.querySelector('.spinner-center');
 
 function countdown() {
@@ -16,13 +16,17 @@ function countdown() {
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
-  daysEl.innerText = days
-  hoursEl.innerText = hours
-  minutesEl.innerText = minutes
-  secondsEl.innerText = seconds
+  daysEl.innerText = days;
+  hoursEl.innerText = formatTime(hours);
+  minutesEl.innerText = formatTime(minutes);
+  secondsEl.innerText = formatTime(seconds);
 }
 
-setInterval(countdown, 1000)
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
 setTimeout(() => {
   spinner.classList.add('d-none');
-}, 1000)
+}, 1000);
